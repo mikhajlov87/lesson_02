78fd3ffd833c249bc459e55b33219c9df96de856
sergei <mikhajlov87@gmail.com>
2017-04-15 21:53:00 +0300

    create drop-down menu

--- main.js
+++ main.js
@@ -1,64 +1,65 @@
-(function () {
+document.addEventListener('DOMContentLoaded', function () {
     'use strict';
-    function createChessboard() {
-        const ZERO = 0;
+
+    let arr = ['list', 'item', 'list-item', 'drop', 'down', 'drop-down'];
+
+    renderButton(arr);
+
+    function renderButton(array) {
         let body = document.body,
-            script = body.querySelector('script'),
-            table = createElem('table'),
-            tHead = createElem('thead'),
-            tBody = createElem('tbody'),
-            tableSellCount = 9,
-            thCaption = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', ''],
-            tr, td, th, i, j;
+            ul = createListItems(array),
+            btn = createElem('button');
 
-        for (i = tableSellCount; i >= ZERO; i--) {
-            tr = createElem('tr');
-            for (j = ZERO; j <= tableSellCount; j++) {
-                switch (true) {
-                    case (i === tableSellCount):
-                        th = createElem('th');
-                        th.textContent = thCaption[j];
-                        tr.appendChild(th);
-                        tHead.appendChild(tr);
-                        break;
+        btn.textContent = 'button';
 
-                    case (i > ZERO && i < tableSellCount):
-                        if (!j || j === tableSellCount) {
-                            th = createElem('th');
-                            th.textContent = i;
-                            tr.appendChild(th);
-                        } else {
-                            td = createElem('td');
-                            td.className = 'white';
-                            if ( !(i % 2) && !(j % 2) ) {
-                                td.className = 'black';
-                            }
-                            if ( (i % 2) && (j % 2) ) {
-                                td.className = 'black';
-                            }
-                            tr.appendChild(td);
-                        }
-                        tBody.appendChild(tr);
-                        break;
+        btn.addEventListener('click', displayListItems);
+        ul.addEventListener('click', changeValue);
 
-                    case (i === ZERO):
-                        th = createElem('th');
-                        th.textContent = thCaption[j];
-                        tr.appendChild(th);
-                        tBody.appendChild(tr);
-                        break;
-                }
-            }
+        body.appendChild(btn);
+        body.appendChild(ul);
+
+    }
+
+    function createListItems(array) {
+        let ul = createElem('ul'),
+            len = array.length,
+            li,
+            i;
+
+        for (i = 0; i < len; i++) {
+            li = createElem('li');
+            li.textContent = array[i];
+            ul.appendChild(li);
         }
 
-        table.appendChild(tHead);
-        table.appendChild(tBody);
-        body.insertBefore(table, script);
+        ul.classList = 'hide';
+        return ul;
+    }
+
+    function changeValue(e) {
+        let target = e.target,
+            btn = document.querySelector('button');
+
+        btn.textContent = target.textContent;
+        hideList();
     }
 
     function createElem(tagName) {
         return document.createElement(tagName);
     }
 
-    createChessboard();
-})();
+    function displayListItems(e) {
+        e.stopPropagation();
+        let ul = document.querySelector('ul');
+
+        ul.classList.toggle('show');
+    }
+
+    function hideList() {
+        let list = document.querySelector('ul');
+
+        list.className = 'hide';
+    }
+
+    document.addEventListener('click', hideList);
+});
